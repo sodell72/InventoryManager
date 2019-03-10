2887a9d840ded9a63e028cc9157feaea661d12ac
Sean O'Dell <sodell72@hotmail.com>
2019-03-09 20:01:08 -0800

    implemented performCommand

--- InventoryManager/Store.cpp
+++ InventoryManager/Store.cpp
@@ -1,2 +1,41 @@
 #include "Store.h"
 
+bool Store::performCommand(std::string command)
+{
+	char commandType = command[0];
+
+	switch (commandType)
+	{
+	case 'B': return performBorrowCommand(command);
+		break;
+	case 'R': return performReturnCommand(command);
+		break;
+	case 'I': return performInventoryCommand(command);
+		break;
+	case 'H': return performHistoryCommand(command);
+	default:
+		cout << "command: \"" << command << "\" is invalid";
+		return false;
+	}
+	return false;
+}
+
+bool Store::performBorrowCommand(std::string command)
+{
+	return false;
+}
+
+bool Store::performReturnCommand(std::string command)
+{
+	return false;
+}
+
+bool Store::performInventoryCommand(std::string command)
+{
+	return false;
+}
+
+bool Store::performHistoryCommand(std::string command)
+{
+	return false;
+}
